a1d8a81a97685379d32ac9068937c12130e9d0e2
Rommert Zijlstra <rommert@thinkbright.nl>
2014-04-02 16:54:01 +0200

    added a mutator for old data + started work on wearDump2 table

--- app.js
+++ app.js
@@ -32,6 +32,33 @@ AWS.config.loadFromPath('./aws.config.json');
 AWS.config.region = "eu-west-1";
 var db = new AWS.DynamoDB({apiVersion: '2012-08-10'});
 
+// Parse the packets from the old Table
+function parseOldPackets(packets) {
+
+  var chunks = [];
+
+  for (var i = 0; i < packets.length; i++) {
+
+    var packet = packets[i].blob.S;
+
+    if (packet !== undefined) {
+
+      packet = JSON.parse(packet);
+
+      // I know.. nested for loops. Needed to normalize the data
+      for (var j = 0; j < packet.length; j++) {
+        var chunk = packet[j];
+
+        chunks.push(chunk);
+      }
+
+    }
+
+  }
+
+  return chunks;
+}
+
 app.get('/', function(req, res){
 
   var params = {
@@ -62,8 +89,67 @@ app.get('/get', function(req, res){
 
   db.scan(params, function(err, data){
     if (err) res.send(err + err.stack);
-    else res.send(JSON.stringify(data).replace(/\\/g, ''));
+    else {
+
+      var packets = data.Items;
+
+      var result = parseOldPackets(packets)
+
+      res.send(result);
+
+    }
   });
+
+});
+
+app.get('/mutate', function(req, res){
+
+  var params = {
+    TableName: 'wearDump',
+    ScanFilter: {
+      timestamp: {
+        ComparisonOperator: 'BETWEEN',
+        AttributeValueList: [
+          { N: '1390000000000' },
+          { N: '1400000000000' }
+        ]
+      }
+    }
+  };
+
+db.scan(params, function(err, data){
+  if (err) res.send(err + err.stack);
+  else {
+
+    var packets = data.Items,
+        result = parseOldPackets(packets);
+
+    // Transmute data
+    for (var i = 0; i < result.length; i++) {
+
+      var chunk = result[i],
+          params = {
+        TableName: 'wearDump2',
+        Item: {
+          id: { S: uuid.v1() },
+          timestamp: { N: Math.round(chunk.timestamp).toString() },
+          name: { S: chunk.name },
+          timestampRaw: { N: Math.round(chunk.timestampRaw).toString() },
+          type: { N: chunk.type.toString() },
+          values: { S: JSON.stringify(chunk.values) }
+        }
+      };
+
+      db.putItem(params, function(err, data){
+        if (err) console.log(err + err.stack);
+        else console.log('#' + params.Item.id + ' saved');
+      });
+
+    }
+
+    res.send('Mutation done.');
+  }
+});  
 
 });
 
